f951d4ce239b254b84d61bd45040a10d8388d4f9
Harmesh Kumar <kumarharmesh@hotmail.com>
2022-11-18 14:37:30 +0530

    second commit

---
+++ Leap.cs
@@ -0,0 +1,8 @@
+using System;
+public class LeapClass
+{
+    public bool Compare(int x ,int y)
+    {
+        return x>y;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+LeapClass lp=new LeapClass();
+bool r=lp.Compare(100,20); 
+Console.WriteLine(r);
