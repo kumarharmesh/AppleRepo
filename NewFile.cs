f03bd3a637354948372a5ea2ba6417d12ef8b50b
Harmesh Kumar <kumarharmesh@hotmail.com>
2023-01-05 22:32:26 +0530

    newfile added

---
+++ NewFile.cs
@@ -0,0 +1,12 @@
+public class Subscriber
+{
+    Message msg=new Message ();
+    public NewFile()
+    {
+        msg.MsgEvent+=handler;
+    }
+    public handler(string m)
+    {
+        Console.WriteLine(m);
+    }
+}
