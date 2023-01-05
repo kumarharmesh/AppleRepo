ef0c7877fdead7a96e16778bb894010e42befb66
Harmesh Kumar <kumarharmesh@hotmail.com>
2023-01-05 19:40:18 +0530

    again added

---
+++ A2.cs
@@ -0,0 +1,14 @@
+public class DateSettings
+{
+    public string GetTime()
+    {
+        return DateTime.Now.ToLongTimeString();
+    }
+}
+public class LeapClass2
+{
+    public bool Compare(int x ,int y)
+    {
+        return x>y;
+    }
+}
